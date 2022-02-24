7907c1e2ba7b6ba37673a5c0567d6986ea6ffb07
John Walbran <32685970+jpwalbran@users.noreply.github.com>
2022-02-23 22:06:12 -0600

    Remove unused imports and empty onInit() methods
    Removed empty onInit() methods from relevant components and then removed extraneous imports to comply with ESLint.

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
+import { Component, Output, EventEmitter, Input } from '@angular/core';
 import { Product } from '../products';
 
 @Component({
@@ -6,12 +6,10 @@ import { Product } from '../products';
   templateUrl: './product-alerts.component.html',
   styleUrls: ['./product-alerts.component.css']
 })
-export class ProductAlertsComponent implements OnInit {
+export class ProductAlertsComponent {
 
   @Input() product!: Product | undefined;
   @Output() notify = new EventEmitter();
   constructor() { }
 
-  ngOnInit(): void {
-  }
 }
